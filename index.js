2ba12466a320ed7159ec40709857e429e4fbb8c5
Andy Edstrom <andyedstrom115@gmail.com>
2020-06-17 18:32:22 -0400

    index.js adding functions

--- index.js
+++ index.js
@@ -1,48 +1,121 @@
-var inquirer = require("inquirer")
+let inquirer = require("inquirer")
 
-var Word = require("./Word.js")
+let Word = require("./Word.js")
 
-var guesses = 10
+let guesses = 10
 let correct = 0
 
-let guessWords = ['bacon','pancakes','potatoes','biscuit', 'frittatas']
+let guessWords = ['bacon', 'pancakes', 'potatoes', 'biscuit', 'frittatas']
 let randomWord
 let chosenword
 
-function startGame(){
+function startGame() {
     console.log("Guess the Breakfast Food!")
 }
 
-function startGame(){
-    randomWord = guessWords[Math.floor(Math.ramdom()*guessWords.length)]
+function choseRanomWord() {
+    randomWord = guessWords[Math.floor(Math.ramdom() * guessWords.length)]
 
     chosenword = new Word(randomWord)
 }
 
-function guessWord () {
-    if (guesses > 0 && points > 5){
-        console.log(chosenword.display())
-    }
+function guessWord() {
+    if (guesses > 0 && points > 5) {
 
-    inquirer.prompt([{
-        name: "txt",
-        message:"Guess a letter",
-        validate: function (str){
-            if (str.length <1) return false;
-        } 
-    }
-]).then(function (guessedLetter){
-    var guess = guessedLetter.txt;
+        console.log(chosenword.display());
 
-    chosenWord.checkGuess(guess);
 
-    if (randomWord.indexOf(guess) === -1) {
-        guesses--;
-        console.log("incorrect" + guesses + " guesses remaining")
-    } else {
-        if (points < 5)
-        console.log('correct')
-        chooseRandomWord();
+        inquirer.prompt([
+            {
+                name: "txt",
+                message: "Guess a letter",
+                validate: function (str) {
+                    if (str.length < 1) return false;
+                }
+            }
+        ]).then(function (guessedLetter) {
+
+            var guess = guessedLetter.txt;
+
+            chosenWord.checkGuess(guess);
+
+            if (randomWord.indexOf(guess) === -1) {
+                guesses--;
+                console.log("incorrect" + guesses + " guesses remaining")
+            }
+            else {
+                if (points < 5) {
+                    console.log('correct')
+                }
+            }
+
+            if (randomWord === chosenword.display()) {
+                console.log(chosenword.display());
+                guesses = 10
+                points++;
+
+                if (points < 5) {
+                    console.log("correct. next word")
+                    chooseRandomWords();
+                }
+                else{
+                    winGame();
+                }
+            }
+            
+            if (guesses === 0)  {
+                loseGame();
+            }
+
+            guessWord();
+
+        });
     }
-})
 }
+
+function loseGame() {
+    console.log("game over.")
+    inquirer.prompt([
+        {
+        name: "confirm",
+        type: "confirm",
+        message:"Play Agian?",
+        default: true
+    }
+])
+    .then(function (inquirerResponse) {
+        if(inquirerResponse.confirm) {
+            guesses = 10;
+            points = 0
+            chooseRandomWords();
+            guessWord();
+        }
+        else {
+            console.log("bye bye");
+            process.exit()
+        }
+    })
+}
+
+function winGame() {
+    console.log("you win.",function(err, data){
+        if (err) {
+            console.log('something went wrong');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    })
+
+    inquirer.prompt([
+        {
+            name: "confirm",
+            type: "confirm",
+            message: "Play again?",
+            default: true
+        }
+    ])
+    .then(function(inquirerResonse))
+}
+        
+
